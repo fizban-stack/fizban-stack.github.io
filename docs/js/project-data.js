600fc7ba02dc39481f419030562e52dbdf879a55
fizbanstack <jimmywells1988@gmail.com>
2025-10-18 22:16:00 +0000

    project update

--- docs/js/project-data.js
+++ docs/js/project-data.js
@@ -2,30 +2,30 @@ const projectData = [
   {
     image: "images/projects/raspberry-pi5",
     alt: "Raspberry Pi 5",
-    title: "Raspberry Pi 5: Attack Box",
-    description: "A Raspberry Pi 5 that I setup to connect to any machine via USB and attack.",
+    title: "Raspberry Pi 5: Tools",
+    description: "A Raspberry Pi 5 that I setup with various tools and a remote connection.  It allows me to plug into any network and have access to installed tools.",
     tags: [
       { text: "Single Board Computer", color: "bg-primary" },
-      { text: "Offensive Cybersecurity", color: "bg-info text-dark" },
+      { text: "Cybersecurity", color: "bg-info text-dark" },
       { text: "Hardware", color: "bg-secondary" }
     ],
     links: {
-      blog: "https://www.blog.wellslabs.org/rasberry-pi-attack-box",
+      blog: "https://www.blog.wellslabs.org/raspberry-pi-5/",
       github: "#"
     }
   },
   {
-    image: "images/projects/project-2.png",
-    alt: "Screenshot of Home Lab Dashboard",
-    title: "Home Lab Dashboard",
-    description: "A custom dashboard built in React to monitor my Proxmox and Docker services in real-time.",
+    image: "images/projects/pwnagotchi.webp",
+    alt: "Screenshot of Pwnagotchi",
+    title: "Pwnagotchi aka Bobby",
+    description: "Pwnagotchi is a wifi hacking tool created with a Raspberry Pi Zero 2W.  My Children came up with the name Bobby.",
     tags: [
       { text: "React", color: "bg-success" },
       { text: "Proxmox", color: "bg-warning text-dark" },
       { text: "Docker", color: "bg-info text-dark" }
     ],
     links: {
-      blog: "https://www.blog.wellslabs.org/..."
+      blog: "https://www.blog.wellslabs.org/bobby"
     }
   }
 ];
